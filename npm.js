2c4c5c57181a358eade8462d193f8904610345a9
isaacs <i@izs.me>
2010-01-12 03:25:14 -0800

    It works. call npm.install(tarball, name) to install the package at "tarball" to "name" in your ~/.node_libraries/ folder, so that you can do require(name) to get the thing that the "lib" member points to.

--- npm.js
+++ npm.js
@@ -1,1 +1,11 @@
+var utils = require("./lib/utils");
+
 exports.install = require("./lib/install");
+
+var registry = {};
+
+exports.set = set;
+exports.get = get;
+
+function set (name, data) { return utils.set(registry, name, data) };
+function get (name) { return utils.get(registry, name) };
