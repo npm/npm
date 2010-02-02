0d02951f7be0324071d0c1d658cac521f0eec188
isaacs <i@izs.me>
2010-02-02 09:43:15 -0800

    Using the wrong tarball for npm installation.

--- install-npm.js
+++ install-npm.js
@@ -1,6 +1,6 @@
 function print (m, cr) { process.stdio.writeError(m+(cr===false?"":"\n")); return print };
 
-require("./npm").install("http://github.com/isaacs/npm/tarball/rewrite", "npm")
+require("./npm").install("http://github.com/isaacs/npm/tarball/master", "npm")
   .addCallback(function () { print("It worked!") })
   .addErrback(function () {
     print
