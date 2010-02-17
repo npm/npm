1de25a991e86d3bb54a3b394211c7594abc6651f
isaacs <i@izs.me>
2010-02-17 02:51:14 -0800

    1. Update to work with the latest node (mostly) 2. Change install so that it just takes the path or URL to a tarball, and reads what it needs from the package.json within. 3. Install things to the proper folders so that installs are versioned.  (Activation and dependency resolution to come later.) Sorry for the huge commit.  A lot changed today.

--- npm.js
+++ npm.js
@@ -9,3 +9,10 @@ exports.get = get;
 
 function set (name, data) { return utils.set(registry, name, data) };
 function get (name) { return utils.get(registry, name) };
+
+var path = require("path");
+
+exports.root = path.join(process.env.HOME, ".node_libraries");
+exports.__defineGetter__("dir", function () { return path.join(exports.root, ".npm") });
+exports.__defineGetter__("tmp", function () { return path.join(exports.dir, ".tmp") });
+
