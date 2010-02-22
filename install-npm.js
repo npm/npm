b0ce742f6f58a84baf6a625e9cbb7113c9730b80
isaacs <i@izs.me>
2010-02-22 00:52:31 -0800

    Make no promises

--- install-npm.js
+++ install-npm.js
@@ -1,11 +1,10 @@
+var sys = require("sys");
+
 function print (m, cr) { process.stdio.writeError(m+(cr===false?"":"\n")); return print };
 
-require("./npm").install("http://github.com/isaacs/npm/tarball/master", "npm")
-  .addCallback(function () { print("It worked!") })
-  .addErrback(function () {
-    print
-      ("Failure!\n")
-      ([].join.call(arguments,"\n"))
-      ("")
-      (new Error().stack);
-  });
+require("./npm").install("http://github.com/isaacs/npm/tarball/master", function (er, ok) {
+  if (er) {
+    sys.error("\nFailed after "+ok.length+" step(s)\n");
+    throw er;
+  } else print("It worked!");
+})
