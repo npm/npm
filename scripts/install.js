ce3c2fa7f168593873c47d1935bee1c98f62b959
isaacs <i@izs.me>
2010-03-01 01:43:22 -0800

    env debuggery

--- scripts/install.js
+++ scripts/install.js
@@ -6,6 +6,10 @@ var fs = require("fs"),
   clijs = require("path").join(process.cwd(), "cli.js"),
   sys = require("sys");
 
+for (var i in process.env) {
+  log("env."+i+"="+JSON.stringify(process.env[i]), "lifecycle");
+}
+
 fs.lstat(bin, function (er, st) {
   if (!er) fs.unlink(bin, linkBin);
   else linkBin();
