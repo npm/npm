582a9f0e6a82022db572d8e82f9c2cee14c84523
isaacs <i@izs.me>
2010-03-01 03:40:29 -0800

    Change around the npm.set command so that it also tracks which versions of a thing it knows about.  Use this newfound power to activate npm after installing it.

--- install-npm.js
+++ install-npm.js
@@ -2,9 +2,18 @@ var sys = require("sys");
 
 function print (m, cr) { process.stdio.writeError(m+(cr===false?"":"\n")); return print };
 
-require("./npm").install("http://github.com/isaacs/npm/tarball/master", function (er, ok) {
+var npm = require("./npm"), sys = require("sys");
+npm.install("http://github.com/isaacs/npm/tarball/master", function (er, ok) {
   if (er) {
     sys.error("\nFailed after "+ok.length+" step(s)\n");
     throw er;
-  } else print("It worked!");
+  }
+  // sys.debug(sys.inspect(npm.get("npm")));
+  npm.activate("npm", npm.get("npm")._versions[0], function (er, ok) {
+    if (er) {
+      sys.error("\nFailed to activate\n");
+      throw er;
+    }
+    print("It worked!");
+  });
 })
