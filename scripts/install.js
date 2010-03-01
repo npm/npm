7a9b18777c616fb445d584df959d16b041e1b090
isaacs <i@izs.me>
2010-03-01 03:40:09 -0800

    Don't debug all the env vars.

--- scripts/install.js
+++ scripts/install.js
@@ -6,10 +6,6 @@ var fs = require("fs"),
   clijs = require("path").join(process.cwd(), "cli.js"),
   sys = require("sys");
 
-for (var i in process.env) {
-  sys.debug("env."+i+"="+JSON.stringify(process.env[i]), "lifecycle");
-}
-
 fs.lstat(bin, function (er, st) {
   if (!er) fs.unlink(bin, linkBin);
   else linkBin();
