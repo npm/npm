1cea59b7da65f84eb73fef76f29826c1d38db521
isaacs <i@izs.me>
2010-03-01 01:35:29 -0800

    Make the scripts executable.

--- scripts/install.js
+++ scripts/install.js
@@ -1,16 +1,22 @@
 #!/usr/local/bin/node
+
 var fs = require("fs"),
   version = process.env["npm.package.version"],
   bin = "/usr/local/bin/npm-"+version,
-  clijs = require("path").join(process.cwd(), "cli.js");
+  clijs = require("path").join(process.cwd(), "cli.js"),
+  sys = require("sys");
+
+sys.puts("1 about to link " + clijs+" to " +bin + "\ncwd:"+process.cwd());
+
 
 fs.lstat(bin, function (er, st) {
-  if (er) fs.unlink(bin, linkBin);
+  if (!er) fs.unlink(bin, linkBin);
   else linkBin();
 });
 
 function linkBin (er) {
   if (er) throw er;
+  sys.puts("about to link " + clijs+" to " +bin);
   fs.symlinkSync(clijs, bin, function (er) {
     if (er) throw er;
   });
