040846bfb1841d7475e51a2bda062320faad5436
isaacs <i@izs.me>
2010-05-06 15:59:40 -0700

    Make sure the man target dir exists before attempting to install docs.  Bug reported by Ryan Grove.

--- install-docs.js
+++ install-docs.js
@@ -4,43 +4,47 @@
 // reflect the current command.
 
 var event = process.env.npm_lifecycle_event
-  , chain = require("./lib/utils/chain")
   , exec = require("./lib/utils/exec")
   , log = require("./lib/utils/log")
   , fs = require("fs")
   , path = require("path")
   , rm = require("./lib/utils/rm-rf")
+  , mkdir = require("./lib/utils/mkdir-p")
+  , manTarget = path.join(process.installPrefix, "share/man/man1")
 
 log(event, "install docs")
 
-fs.readdir(path.join(process.cwd(), "man"), function (er, docs) {
-  log(path.join(process.cwd(), "man"), "readdir")
-  if (er) return
-  ;(function R (doc) {
-    if (!doc) return
-    if (doc === "." || doc === "..") return R(docs.pop())
-    var target = path.join(process.installPrefix, "share/man/man1", "npm-"+doc)
-    target = target.replace(/npm-npm\.1$/, "npm.1")
-    log(target, event)
-    switch (event) {
-      case "activate":
-        rm( target
-          , function () {
-              fs.symlink
-                ( path.join(process.cwd(), "man", doc)
-                , target
-                , function (er, ok) {
-                    if (er) throw er
-                    R(docs.pop())
-                  }
-                )
-            }
-          )
-      break
-      case "deactivate":
-        rm( target, function (er) { R(docs.pop()) })
-      break
-      default: throw new Error("invalid state"); break
-    }
-  })(docs.pop())
+mkdir(manTarget, function (er) {
+  if (er) throw er
+  fs.readdir(path.join(process.cwd(), "man"), function (er, docs) {
+    log(path.join(process.cwd(), "man"), "readdir")
+    if (er) return
+    ;(function R (doc) {
+      if (!doc) return
+      if (doc === "." || doc === "..") return R(docs.pop())
+      var target = path.join(manTarget, "npm-"+doc)
+      target = target.replace(/npm-npm\.1$/, "npm.1")
+      log(target, event)
+      switch (event) {
+        case "activate":
+          rm( target
+            , function () {
+                fs.symlink
+                  ( path.join(process.cwd(), "man", doc)
+                  , target
+                  , function (er, ok) {
+                      if (er) throw er
+                      R(docs.pop())
+                    }
+                  )
+              }
+            )
+        break
+        case "deactivate":
+          rm( target, function (er) { R(docs.pop()) })
+        break
+        default: throw new Error("invalid state"); break
+      }
+    })(docs.pop())
+  })
 })
