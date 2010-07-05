9e9e4b3aaef55c275ec12c697064e074c9855104
isaacs <i@izs.me>
2010-07-05 11:04:28 -0700

    Make install script executable, and also report success or die on failure

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-mkdir npm
-cd npm
-curl -L http://github.com/isaacs/npm/tarball/master | tar xz --strip 1
-make
+mkdir npm \
+  && cd npm \
+  && curl -L http://github.com/isaacs/npm/tarball/master | tar xz --strip 1 \
+  && make \
+  && cd .. \
+  && rm -rf npm \
+  && echo "It worked"
