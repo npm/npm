e86037de1fe2f5e34765719d4d9ac1afb921343b
isaacs <i@izs.me>
2010-07-25 12:36:05 -0700

    Fix tar usage so that it works on BSD

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 mkdir npm \
   && cd npm \
-  && curl -L http://github.com/isaacs/npm/tarball/master | tar xz --strip 1 \
+  && curl -L http://github.com/isaacs/npm/tarball/master | tar xzf - --strip-components=1 \
   && make \
   && cd .. \
   && rm -rf npm \
