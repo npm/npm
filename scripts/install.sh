fe06c36f420e7c88469316014c2dd7bf0c4574eb
isaacs <i@izs.me>
2010-09-13 14:28:19 -0700

    Be respectful of weird places where tar might live.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 r=${RANDOM-$(date +%s)}
+tar=${TAR-$(if (which gtar 2>/dev/null) ; then which gtar ; else echo "tar" ; fi)}
 mkdir npm-$r \
   && cd npm-$r \
-  && curl -L http://github.com/isaacs/npm/tarball/master | tar xzf - --strip-components=1 \
+  && curl -L http://github.com/isaacs/npm/tarball/master | $tar xzf - --strip-components=1 \
   && make uninstall install \
   && cd .. \
   && rm -rf npm-$r \
