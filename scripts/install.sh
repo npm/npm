0406142d771362d87da4d77ce92a072e32e8fb8a
isaacs <i@izs.me>
2010-09-13 14:50:22 -0700

    More solarisy fixes to the easy install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 r=${RANDOM-$(date +%s)}
-tar=${TAR-$(if (which gtar 2>/dev/null) ; then which gtar ; else echo "tar" ; fi)}
+tar=${TAR-$(if which gtar 1>/dev/null 2>/dev/null; then echo "gtar" ; else echo "tar" ; fi)}
 mkdir npm-$r \
   && cd npm-$r \
   && curl -L http://github.com/isaacs/npm/tarball/master | $tar xzf - --strip-components=1 \
