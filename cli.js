216d89f90312564a6837b716313f504f37700677
isaacs <i@izs.me>
2010-09-21 16:36:13 -0700

    Support "./foo" for root configs, and parse cli with the same logic that ini and env confs go through

--- cli.js
+++ cli.js
@@ -39,9 +39,6 @@ while (arg = argv.shift()) {
     if (key === "help") conf[key] = true, key = null
     flagsDone = (key === "")
   } else if (key) {
-    if (arg === "false" || arg === "null") arg = JSON.parse(arg)
-    else if ( arg === "undefined" ) arg = undefined
-    else if (!isNaN(arg)) arg = +arg
     conf[key] = arg
     key = null
   } else arglist.push(arg)
