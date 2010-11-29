18b95cf2978be532e999430ce9a2ff3654140e1c
isaacs <i@izs.me>
2010-11-29 15:12:46 -0800

    Add COMP environment vars. Log to npm-completion.log

--- npm-completion.sh
+++ npm-completion.sh
@@ -2,30 +2,33 @@
 # npm command completion script
 #
 # Install Method 1: Automatic
-# Drop this file in /etc/bash-completion.d or /usr/local/etc/bash-completion.d
+# Put this file in /etc/bash-completion.d or /usr/local/etc/bash-completion.d
 # or wherever bash-completion scripts are sourced on your system.
 #
 # Install Method 2: Generic
-# Put this in your .bashrc or whatever file you run when you log into a machine:
+# Put this in .bashrc or whatever file you run when you log into a machine:
 # . path/to/npm-completion.sh
 #
-# Then use the tab key to complete commands, which executes the "npm completion"
-# command.
+# Then use the tab key, which executes the "npm completion" command.
 #
-# Note that command completion is very rudimentary and incomplete (harhar) as of
-# this time.  Patches welcome!
-#
-# --i
+# Special thanks to Evan Meagher for making the npm completion command
+# much more useful and complete.
 
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/@/}
 __npm_completion () {
   COMPREPLY=()
   local cur prev opts
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
   # opts=$(npm complete --loglevel silent --color false -- "$cur")
-  COMPREPLY=( $(COMP_CWORD=${COMP_CWORD} \
-                npm completion --color false \
-                -- "${COMP_WORDS[@]}" ) )
+  COMPREPLY=( $(COMP_CWORD=$COMP_CWORD \
+                COMP_LINE=$COMP_LINE \
+                COMP_POINT=$COMP_POINT \
+                COMP_WORDBREAKS=$COMP_WORDBREAKS \
+                COMP_WORDS="${COMP_WORDS[@]}" \
+                npm completion --color false --loglevel warn \
+                -- "${COMP_WORDS[@]}" \
+                2>>./npm-completion.log ) )
   return $?
 }
 
