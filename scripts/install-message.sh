d521bd755bae2217d96fc078ed310578b1aecf87
isaacs <i@izs.me>
2010-12-05 22:12:31 -0800

    a little message on package installation

---
+++ scripts/install-message.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "x$npm_config_loglevel" = "xsilent" ]; then
+  exit
+fi
+
+cat <<MESSAGE
+
+Thanks for installing version $npm_package_version of npm.
+
+Last few items from the changelog:
+
+`tail -n 20 doc/changelog.md`
+
+MESSAGE
