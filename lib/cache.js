npm/npm

commit cafea69f0fea84b48282abd3be251426092079ec

Add cache completion for cache (ls|clean)
I forgot that I'd written this handy little find util.  I don't for the
life of me remember what it was for, and got halfway through writing
another one when I decided that it belonged in a separate file.

I told vim to ":e lib/utils/find.js", only to be surprised to find
the very utility that I'd meant to write.

It's a bit like finding a $20 in the pocket of a jacket that has been
ignored all summer.

lib/cache.js
@@ -47,6 +47,7 @@ var mkdir = require("./utils/mkdir-p")
   , sys = require("./utils/sys")
   , output = require("./utils/output")
   , sha = require("./utils/sha")
+  , find = require("./utils/find")
 
 cache.usage = "npm cache add <tarball file>"
             + "\nnpm cache add <folder>"
@@ -60,17 +61,30 @@ cache.completion = function(args, index, cb) {
     , getCompletions = require("./utils/completion/get-completions")
     , subcmdList = ["add", "clean", "ls"]
     , subcmd = args[0] || ""
+    , onSubCmd = index === 2
 
-  if (subcmdList.indexOf(subcmd) !== -1) {
-    if (subcmd === "add") {
-      remotePkgs(args.slice(1), index - 1, true, false, false, cb)
-    } else if (subcmd === "clean") {
-      ls_("", npm.cache, function (er, cached) {
-        if (er) return cb(er)
-        cb(null, getCompletions(args[1] || "", cached))
-      })
+  if (onSubCmd) return cb(null, getCompletions(subcmd, subcmdList))
+  if (subcmd === "add") {
+    return remotePkgs(args.slice(1), index - 1, true, false, false, cb)
+  }
+  // at this point, the only args that matter are after the subcmd
+  index -= 3
+  args = args.slice(1)
+  var p = args.join("/").split("@").join("/").split("/")
+    , cur = index < args.length ? p.pop() : ""
+  p = p.join("/")
+
+  // p is what we should ls_ for, cur is what we filter on
+  return ls_(p, 3, function (er, files) {
+    if (files) {
+      files = getCompletions((p ? p+"/" : "")+cur, files)
     }
-  } else if (index < 3) cb(null, getCompletions(subcmd, subcmdList))
+    if (!files || !files.length) {
+      er = new Error("nothing found")
+      files = null
+    }
+    cb(er, files)
+  })
 }
 
 function cache (args, cb) {
@@ -105,55 +119,37 @@ function read (name, ver, cb) {
   })
 }
 
-// npm cache ls [<pkg>[@<ver>]]
+// npm cache ls [<path>]
 function ls (args, cb) {
-  if (args[0] && args[0].indexOf("@") !== -1) {
-    args = args[0].split("@")
-  }
-  var show = path.join.apply(path, args)
-    , read = npm.cache
-    , msg = "cache ls"+(show?" "+show:"")
-  read = path.join(read, show)
-  ls_(show, read, function(er, files) {
+  args = args.join("/").split("@").join("/")
+  if (args.substr(-1) === "/") args = args.substr(0, args.length - 1)
+  ls_(args, function(er, files) {
     files = files || []
-    var prefix = (show ? show + "/" : "")
-    output.write(npm.config.get("outfd"), prefix+files.join("\n"+prefix), cb_)
+    output.write(npm.config.get("outfd"), files.join("\n"), cb_)
     function cb_ () { cb(null, files) }
   })
 }
 
 // Calls cb with list of cached pkgs matching show.
-function ls_ (target, dir, cb) {
+function ls_ (req, depth, cb) {
+  if (typeof cb !== "function") cb = depth, depth = 1
   mkdir(npm.cache, function (er) {
     if (er) return log.er(cb, "no cache dir")(er)
-    fs.readdir(dir, function (er, files) {
-      if (er) {
-        log("nothing found", "cache")
-        return cb()
-      }
-      files = files.filter(function (f) { return f !== "." && f !== ".." })
-      if (!files.length) {
-        log("nothing found", "cache")
-        return cb()
-      }
-      cb(null, files)
+    find(path.join(npm.cache, req), null, depth, function (er, files) {
+      if (er) return cb(er)
+      return cb(null, files.map(function (f) {
+        return f.substr(npm.cache.length + 1)
+      }))
     })
   })
 }
-// npm cache clean [<pkg> [<ver>]]
+// npm cache clean [<path>]
 function clean (args, cb) {
   if (!cb) cb = args, args = []
   if (!args) args = []
-  if (args[0] && args[0].indexOf("@") !== -1) {
-    args = args[0].split("@")
-  }
-  var pkg = args.shift()
-    , ver = args.shift()
-    , clean = npm.cache
-  if (pkg) clean = path.join(clean, pkg)
-  if (ver) clean = path.join(clean, ver)
-  cb = log.er(cb, "Could not remove "+clean)
-  rm(clean, cb)
+  args = args.join("/").split("@").join("/")
+  if (args.substr(-1) === "/") args = args.substr(0, args.length - 1)
+  rm(path.join(npm.cache, path.normalize(args)), cb)
 }
 
 // npm cache add <tarball-url>

lib/utils/find.js
@@ -7,35 +7,46 @@ module.exports = find
 var fs = require("./graceful-fs")
   , asyncMap = require("./async-map")
   , path = require("path")
+  , log = require("./log")
 
-function find (dir, filter, cb) {
-  if (!cb) cb = filter, filter = null
+function find (dir, filter, depth, cb) {
+  if (typeof cb !== "function") cb = depth, depth = Infinity
+  if (typeof cb !== "function") cb = filter, filter = null
   if (filter instanceof RegExp) filter = reFilter(filter)
+  if (typeof filter === "string") filter = strFilter(filter)
   if (!Array.isArray(dir)) dir = [dir]
-  asyncMap(dir, findDir(filter), cb)
+  asyncMap(dir, findDir(filter, depth), cb)
 }
-function findDir (filter) { return function (dir, cb) {
+function findDir (filter, depth) { return function (dir, cb) {
+  log([dir, depth], "dir, depth")
   fs.stat(dir, function (er, stats) {
     // don't include missing files, but don't abort either
     if (er) return cb()
-    if (!stats.isDirectory()) return cb(new Error(
-      "Not a dir, can't walk:"+dir))
+    if (!stats.isDirectory()) return findFile(dir, filter, depth)("", cb)
+    if (filter && !filter(dir)) return cb(er, [])
+    if (depth <= 0) return cb(null, [dir+"/"])
+    cb = (function (cb) { return function (er, f) {
+      cb(er, [dir+"/"].concat(f))
+    }})(cb)
     fs.readdir(dir, function (er, files) {
       if (er) return cb(er)
-      asyncMap(files, findFile(dir, filter), cb)
+      asyncMap(files, findFile(dir, filter, depth - 1), cb)
     })
   })
 }}
-function findFile (dir, filter) { return function (f, cb) {
+function findFile (dir, filter, depth) { return function (f, cb) {
   f = path.join(dir, f)
   fs.stat(f, function (er, s) {
     // don't include missing files, but don't abort either
     if (er) return cb()
-    if (s.isDirectory()) return find(f, cb)
+    if (s.isDirectory()) return find(f, filter, depth, cb)
     if (!filter || filter(f)) cb(null, f)
     else cb()
   })
 }}
 function reFilter (re) { return function (f) {
   return f.match(re)
+}}
+function strFilter (s) { return function (f) {
+  return f.indexOf(s) === 0
 }}

commit 656b642f3dc83710d53f408d050e329c4491f285

Fix bug introduced in cafea69f0fea84b48282 that broke `npm ls`.

lib/cache.js
@@ -135,10 +135,13 @@ function ls_ (req, depth, cb) {
   if (typeof cb !== "function") cb = depth, depth = 1
   mkdir(npm.cache, function (er) {
     if (er) return log.er(cb, "no cache dir")(er)
-    find(path.join(npm.cache, req), null, depth, function (er, files) {
+    function dirFilter (f, type) {
+      return type === "dir" && f && f !== npm.cache + "/"
+    }
+    find(path.join(npm.cache, req), dirFilter, depth, function (er, files) {
       if (er) return cb(er)
       return cb(null, files.map(function (f) {
-        return f.substr(npm.cache.length + 1)
+        return f.substr(npm.cache.length + 1).replace("/", "")
       }))
     })
   })