1b0194a1354249cb87179da2030e556dcc687e0a
isaacs <i@izs.me>
2011-01-09 22:59:44 -0800

    A test of .npmignore file behavior

---
+++ test/packages/npmignore/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+if [[ $npm_package_version == "9999.0.0-LINK-"* ]]; then
+  echo "link package, skipping test"
+  exit 0
+fi
+
+[ -f ./foo/baz ] \
+  && ! [ -f ./foo/bar ] \
+  && [ -f ./.dot-keeper ] \
+  && ! [ -f ./.dot-glob-a ]
