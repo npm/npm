1287c7fd4481c921881a39ed27fee7965e3b241a
isaacs <i@izs.me>
2011-04-30 10:55:01 -0700

    Read contributors out of AUTHORS file, if present

--- bin/read-package-json.js
+++ bin/read-package-json.js
@@ -3,16 +3,20 @@ if (argv.length < 3) {
   console.error("Usage: read-package.json <file> [<fields> ...]")
   process.exit(1)
 }
+
 var fs = require("fs")
   , file = argv[2]
-  , data = JSON.parse(fs.readFileSync(file, "utf8"))
+  , readJson = require("../lib/utils/read-json")
 
-if (argv.length === 3) console.log(data)
-else argv.slice(3).forEach(function (field) {
-  field = field.split(".")
-  var val = data
-  field.forEach(function (f) {
-    val = val[f]
+readJson(file, function (er, data) {
+  if (er) throw er
+  if (argv.length === 3) console.log(data)
+  else argv.slice(3).forEach(function (field) {
+    field = field.split(".")
+    var val = data
+    field.forEach(function (f) {
+      val = val[f]
+    })
+    console.log(val)
   })
-  console.log(val)
 })
