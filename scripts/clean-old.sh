499634dfde57bb49c0450bfab7e1c448a4e672e4
Iain Sproat <iainsproat@gmail.com>
2011-06-16 03:24:24 -0700

    Added quotation marks around $node to allow node.js paths with whitespace to be supported

--- scripts/clean-old.sh
+++ scripts/clean-old.sh
@@ -29,12 +29,12 @@ else
   fi
 
 
-  PREFIX=`dirname $node`
+  PREFIX=`dirname "$node"`
   PREFIX=`dirname $PREFIX`
   echo "prefix=$PREFIX"
   PREFIXES=$PREFIX
 
-  altprefix=`$node -e process.installPrefix`
+  altprefix=`"$node" -e process.installPrefix`
   if ! [ "x$altprefix" = "x" ] && ! [ "x$altprefix" = "x$PREFIX" ]; then
     echo "altprefix=$altprefix"
     PREFIXES="$PREFIX $altprefix"
