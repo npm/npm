c2c23d60f82e850f64404c02b41f5f3ba31497ae
Brandon Benvie <brandon@bbenvie.com>
2012-02-01 21:42:13 -0500

    Change npm.bat to handle a primitive command loop on Windows

--- bin/npm.cmd
+++ bin/npm.cmd
@@ -1,6 +1,32 @@
-:: Created by npm, please don't edit manually.
-@IF EXIST "%~dp0"\"node.exe" (
-  "%~dp0"\"node.exe"  "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
-) ELSE (
-  node  "%~dp0\.\node_modules\npm\bin\npm-cli.js" %*
-)
+@ECHO off
+
+:: Figure out the best way to invoke node and npm
+IF EXIST "%~dp0"\"node.exe" SET NODE="%~dp0"\"node.exe" ELSE SET NODE=node
+SET NPM="%~dp0\.\node_modules\npm\bin\npm-cli.js"
+
+:: Initial invokation, forwarding parameters
+CMD /C %NODE% %NPM% %*
+
+:: Exit if this script was invoked with parameters
+IF !%1==! GOTO INTERACTIVE ELSE GOTO :EOF
+
+:: Go into command loop if given no params
+:INTERACTIVE
+ECHO.
+ECHO.
+ECHO # Enter [Q] to quit #
+ECHO.
+
+:LOOP
+:: Clear and then get next command
+SET COMMAND=
+SET /P COMMAND="npm> "
+
+:: Exit on "q"
+IF "%COMMAND%"=="q" GOTO :EOF
+
+:: Run npm with given command
+ECHO.
+CMD /C %NODE% %NPM% %COMMAND%
+ECHO.
+GOTO LOOP
