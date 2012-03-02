npm/npm

commit 417a81e7818c86303ad40aff458eef3bb8f70368

Bundle nested bundleDependencies properly

lib/utils/tar.js
@@ -580,7 +580,11 @@ function makeList_ (dir, pkg, exList, dfc, cb) {
 
     if (path.basename(dir) === "node_modules"
         && pkg.path === path.dirname(dir)
-        && dfc) { // do fancy crap
+        // do fancy crap
+        && dfc
+        // not already part of a bundled dependency
+        && path.basename(path.dirname(pkg.path)) !== "node_modules") {
+      log.warn(dir, "doing fancy crap")
       files = filterNodeModules(files, pkg)
     } else {
       // If a directory is excluded, we still need to be

commit 3d889e1955845c98e4c4368ecde6f04948f57a0d

Fix for minor bug introduced by 417a81e7818c86303ad40aff458eef3bb8f70368

lib/utils/tar.js
@@ -583,7 +583,9 @@ function makeList_ (dir, pkg, exList, dfc, cb) {
         // do fancy crap
         && dfc
         // not already part of a bundled dependency
-        && path.basename(path.dirname(pkg.path)) !== "node_modules") {
+        && (path.basename(path.dirname(pkg.path)) !== "node_modules"
+          // unless it's the root
+          || pkg.path === npm.prefix)) {
       log.verbose(dir, "doing fancy crap")
       files = filterNodeModules(files, pkg)
     } else {