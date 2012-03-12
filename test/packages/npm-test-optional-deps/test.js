5e7fa914ba5d07bafb47a47b097b4b5ba754826f
isaacs <i@izs.me>
2012-03-12 16:09:25 -0700

    correct assert method name

--- test/packages/npm-test-optional-deps/test.js
+++ test/packages/npm-test-optional-deps/test.js
@@ -3,6 +3,6 @@ var assert = require("assert")
 var path = require("path")
 
 // sax should be the only dep that ends up installed
-assert.deepEquals(fs.readdirSync(path.resolve(__dirname, "node_modules")
-                                ,["sax"]))
-assert.equals(require("sax/package.json").version, "0.3.5")
+assert.deepEqual(fs.readdirSync(path.resolve(__dirname, "node_modules")
+                               ,["sax"]))
+assert.equal(require("sax/package.json").version, "0.3.5")
