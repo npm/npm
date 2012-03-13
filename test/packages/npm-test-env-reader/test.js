b69adc9ca948afb349f9ac946550159aed69b8dd
isaacs <i@izs.me>
2012-03-12 18:49:54 -0700

    Fix failing tests

--- test/packages/npm-test-env-reader/test.js
+++ test/packages/npm-test-env-reader/test.js
@@ -1,6 +1,6 @@
 var envs = []
-for (var e in path.env) {
-  if (e.match(/npm|^path$/i)) envs.push(e + '=' + path.env[e])
+for (var e in process.env) {
+  if (e.match(/npm|^path$/i)) envs.push(e + '=' + process.env[e])
 }
 envs.sort(function (a, b) {
   return a === b ? 0 : a > b ? -1 : 1
--- test/packages/npm-test-optional-deps/test.js
+++ test/packages/npm-test-optional-deps/test.js
@@ -3,6 +3,7 @@ var assert = require("assert")
 var path = require("path")
 
 // sax should be the only dep that ends up installed
-assert.deepEqual(fs.readdirSync(path.resolve(__dirname, "node_modules")
-                               ,["sax"]))
+
+var dir = path.resolve(__dirname, "node_modules")
+assert.deepEqual(fs.readdirSync(dir), ["sax"])
 assert.equal(require("sax/package.json").version, "0.3.5")
