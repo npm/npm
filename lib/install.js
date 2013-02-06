npm/npm

commit 4fdd3c57caf6f64d7552e6fb518d615265d30d5e

Guard against RangeError in findPeerInvalid

lib/install.js
@@ -181,13 +181,17 @@ function findPeerInvalid (where, cb) {
   readInstalled(where, function (er, data) {
     if (er) return cb(er)
 
-    cb(null, findPeerInvalid_(data.dependencies))
+    cb(null, findPeerInvalid_(data.dependencies, []))
   })
 }
 
-function findPeerInvalid_ (packageMap) {
+function findPeerInvalid_ (packageMap, fpiSeen) {
   for (var packageName in packageMap) {
     var pkg = packageMap[packageName]
+    if (fpiSeen.indexOf(pkg) !== -1)
+      return null
+
+    fpiSeen.push(pkg)
 
     if (pkg.peerInvalid) {
       var peersDepending = {};
@@ -201,10 +205,8 @@ function findPeerInvalid_ (packageMap) {
       return { name: pkg.name, peersDepending: peersDepending }
     }
 
-    var peerInvalidDep = pkg.dependencies && findPeerInvalid_(pkg.dependencies)
-    if (peerInvalidDep) {
-      return peerInvalidDep
-    }
+    if (pkg.dependencies)
+      return findPeerInvalid_(pkg.dependencies, fpiSeen)
   }
 
   return null

commit 3f67ce695968fc2068f273e01e8ce8d2e6bb7a2d

install: Fix regression introduced in 4fdd3c5

lib/install.js
@@ -185,13 +185,14 @@ function findPeerInvalid (where, cb) {
   })
 }
 
-function findPeerInvalid_ (packageMap, fpiSeen) {
+function findPeerInvalid_ (packageMap, fpiList) {
+  if (fpiList.indexOf(packageMap) !== -1)
+    return
+
+  fpiList.push(packageMap)
+
   for (var packageName in packageMap) {
     var pkg = packageMap[packageName]
-    if (fpiSeen.indexOf(pkg) !== -1)
-      return null
-
-    fpiSeen.push(pkg)
 
     if (pkg.peerInvalid) {
       var peersDepending = {};
@@ -205,8 +206,11 @@ function findPeerInvalid_ (packageMap, fpiSeen) {
       return { name: pkg.name, peersDepending: peersDepending }
     }
 
-    if (pkg.dependencies)
-      return findPeerInvalid_(pkg.dependencies, fpiSeen)
+    if (pkg.dependencies) {
+      var invalid = findPeerInvalid_(pkg.dependencies, fpiList)
+      if (invalid)
+        return invalid
+    }
   }
 
   return null

test/tap/peer-deps-invalid.js
@@ -10,13 +10,15 @@ test("installing dependencies that having conflicting peerDependencies", functio
   process.chdir(__dirname + "/peer-deps-invalid")
 
   npm.load(function () {
-    npm.install(".", function (err) {
+    npm.commands.install([], function (err) {
       if (!err) {
         t.fail("No error!")
+        process.exit(1)
         return
       }
 
       t.equal(err.code, "EPEERINVALID")
+      process.exit(0)
     })
   })
 })

test/tap/peer-deps-without-package-json.js
@@ -6,7 +6,6 @@ var npm = require("../../")
 var peerDepsTestUrl = "https://gist.github.com/raw/3971128/3f6aa37b4fa1186c2f47da9b77dcc4ec496e3483/index.js"
 
 test("installing a peerDependencies-using package without a package.json present (GH-3049)", function (t) {
-  t.plan(2)
 
   rimraf.sync(__dirname + "/peer-deps-without-package-json/node_modules")
   fs.mkdirSync(__dirname + "/peer-deps-without-package-json/node_modules")
@@ -17,11 +16,14 @@ test("installing a peerDependencies-using package without a package.json present
       if (err) {
         t.fail(err)
         t.end()
+        process.exit(1)
         return
       }
 
       t.ok(fs.existsSync(__dirname + "/peer-deps-without-package-json/node_modules/npm-test-peer-deps-file"))
       t.ok(fs.existsSync(__dirname + "/peer-deps-without-package-json/node_modules/dict"))
+      t.end()
+      process.exit(0)
     })
   })
 })