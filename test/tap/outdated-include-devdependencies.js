427ea5ef75c7a29899c0faf98c32f0653a0bec0e
Jason Diamond <jason@diamond.name>
2013-03-17 15:18:50 -0700

    include devDependencies in outdated and update

---
+++ test/tap/outdated-include-devdependencies.js
@@ -0,0 +1,26 @@
+var test = require("tap").test
+var rimraf = require("rimraf")
+var npm = require("../../")
+
+var mr = require("npm-registry-mock")
+
+// config
+var port = 1331
+var address = "http://localhost:" + port
+var pkg = __dirname + '/outdated-include-devdependencies'
+
+
+test("includes devDependencies in outdated", function (t) {
+  rimraf.sync(pkg + "/node_modules")
+  process.chdir(pkg)
+
+  mr(port, function (s) {
+    npm.load({registry: address}, function () {
+      npm.outdated(function (er, d) {
+        t.equal("1.5.1", d[0][3])
+        s.close()
+        t.end()
+      })
+    })
+  })
+})
