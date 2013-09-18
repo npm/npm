721e0a96ce241b8a943aa242ede0c4a55fbfc945
Robert Kowalski <rok@kowalski.gd>
2013-09-18 23:09:26 +0200

    housekeeping: cleanup tests

--- test/tap/outdated-include-devdependencies.js
+++ test/tap/outdated-include-devdependencies.js
@@ -1,5 +1,4 @@
 var test = require("tap").test
-var rimraf = require("rimraf")
 var npm = require("../../")
 
 var mr = require("npm-registry-mock")
@@ -11,9 +10,7 @@ var pkg = __dirname + '/outdated-include-devdependencies'
 
 
 test("includes devDependencies in outdated", function (t) {
-  rimraf.sync(pkg + "/node_modules")
   process.chdir(pkg)
-
   mr(port, function (s) {
     npm.load({registry: address}, function () {
       npm.outdated(function (er, d) {
