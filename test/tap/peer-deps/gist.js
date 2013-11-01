f8c2f9f80bcb68fc77b993931d389adb13e00eff
Robert Kowalski <rok@kowalski.gd>
2013-11-01 17:52:43 +0100

    migrate test to tap

---
+++ test/tap/peer-deps/gist.js
@@ -0,0 +1,11 @@
+/**package
+ * { "name": "npm-test-peer-deps-file"
+ * , "main": "index.js"
+ * , "version": "1.2.3"
+ * , "description":"No package.json in sight!"
+ * , "peerDependencies": { "request": "0.9.x" }
+ * , "dependencies": { "underscore": "1.3.1" }
+ * }
+ **/
+
+module.exports = "I'm just a lonely index, naked as the day I was born."
