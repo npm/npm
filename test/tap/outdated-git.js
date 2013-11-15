npm/npm

commit 2f7fd625a0f2eaf59789ab13bd15bba907ae166d

Discover new versions with outdated
Add versions that are not matching the version specified in the
package.json as 'latest', at cost of one request more per package.

Show versions that are specified by the package.json as 'wanted'

Fixes #1428

lib/outdated.js
@@ -45,6 +45,7 @@ function makePretty (p) {
     , dir = path.resolve(p[0], "node_modules", dep)
     , has = p[2]
     , want = p[3]
+    , latest = p[4]
 
   // XXX add --json support
   // Should match (more or less) the output of ls --json
@@ -61,8 +62,10 @@ function makePretty (p) {
   if (!npm.config.get("global")) {
     dir = path.relative(process.cwd(), dir)
   }
-  return dep + "@" + want + " " + dir
+  return dep + " " + dir
        + " current=" + (has || "MISSING")
+       + " wanted=" + want
+       + " latest=" + latest
 }
 
 function outdated_ (args, dir, parentHas, cb) {
@@ -154,29 +157,35 @@ function shouldUpdate (args, dir, dep, has, req, cb) {
              , cb )
   }
 
-  function doIt (shouldHave) {
-    cb(null, [[ dir, dep, curr && curr.version, shouldHave, req ]])
+  function doIt (wanted, latest) {
+    cb(null, [[ dir, dep, curr && curr.version, wanted, latest, req ]])
   }
 
   if (args.length && args.indexOf(dep) === -1) {
     return skip()
   }
 
-  // so, we can conceivably update this.  find out if we need to.
-  cache.add(dep, req, function (er, d) {
-    // if this fails, then it means we can't update this thing.
-    // it's probably a thing that isn't published.
-    if (er) return skip()
+  var registry = npm.registry
+  // search for the latest package
+  registry.get(dep + "/latest", function (er, l) {
+    if (er) return cb()
+    // so, we can conceivably update this.  find out if we need to.
+    cache.add(dep, req, function (er, d) {
+      // if this fails, then it means we can't update this thing.
+      // it's probably a thing that isn't published.
+      if (er) return skip()
 
-    // check that the url origin hasn't changed (#1727) and that
-    // there is no newer version available
-    var dFromUrl = d._from && url.parse(d._from).protocol
-    var cFromUrl = curr && curr.from && url.parse(curr.from).protocol
+      // check that the url origin hasn't changed (#1727) and that
+      // there is no newer version available
+      var dFromUrl = d._from && url.parse(d._from).protocol
+      var cFromUrl = curr && curr.from && url.parse(curr.from).protocol
 
-    if (!curr || dFromUrl && cFromUrl && d._from !== curr.from
-        || d.version !== curr.version)
-      doIt(d.version)
-    else
-      skip()
+      if (!curr || dFromUrl && cFromUrl && d._from !== curr.from
+          || d.version !== curr.version
+          || d.version !== l.version)
+        doIt(d.version, l.version)
+      else
+        skip()
+    })
   })
 }

test/tap/outdated-new-versions.js
@@ -1,0 +1,25 @@
+var test = require("tap").test
+var npm = require("../../")
+
+var mr = require("npm-registry-mock")
+
+// config
+var port = 1331
+var address = "http://localhost:" + port
+var pkg = __dirname + '/outdated-new-versions'
+
+
+test("dicovers new versions in outdated", function (t) {
+  process.chdir(pkg)
+
+  mr(port, function (s) {
+    npm.load({registry: address}, function () {
+      npm.outdated(function (er, d) {
+        t.equal("1.5.1", d[0][4]) // dependencies
+        t.equal("2.27.0", d[1][4]) // devDependencies
+        s.close()
+        t.end()
+      })
+    })
+  })
+})

commit 46c7ebe5e222d5a4478cc5967585c36fb22dc6a0

Always update git urls
Do directly an update if a git url is specified in the package.json
For outdated, print wanted=git and latest=git

Additionally, fixing a small, still unknown  bug in update,
regarding the positions of where, latest and req, introduced
by 2f7fd625a0f2

Fixes #1727

lib/install.js
@@ -72,6 +72,7 @@ var npm = require("./npm.js")
   , mkdir = require("mkdirp")
   , lifecycle = require("./utils/lifecycle.js")
   , archy = require("archy")
+  , isGitUrl = require("./utils/is-git-url.js")
 
 function install (args, cb_) {
   var hasArguments = !!args.length
@@ -690,11 +691,19 @@ function targetResolver (where, context, deps) {
         return cb(null, [])
       }
 
+      // if the target is a git repository, we always want to fetch it
+      var isGit = false
+        , maybeGit = what.split("@").pop()
+
+      if (maybeGit)
+        isGit = isGitUrl(url.parse(maybeGit))
+
       if (!er &&
           data &&
           !context.explicit &&
           context.family[data.name] === data.version &&
-          !npm.config.get("force")) {
+          !npm.config.get("force") &&
+          !isGit) {
         log.info("already installed", data.name + "@" + data.version)
         return cb(null, [])
       }

lib/outdated.js
@@ -25,6 +25,7 @@ var path = require("path")
   , asyncMap = require("slide").asyncMap
   , npm = require("./npm.js")
   , url = require("url")
+  , isGitUrl = require("./utils/is-git-url.js")
 
 function outdated (args, silent, cb) {
   if (typeof cb !== "function") cb = silent, silent = false
@@ -164,6 +165,9 @@ function shouldUpdate (args, dir, dep, has, req, cb) {
   if (args.length && args.indexOf(dep) === -1) {
     return skip()
   }
+
+  if (isGitUrl(url.parse(req)))
+    return doIt("git", "git")
 
   var registry = npm.registry
   // search for the latest package

lib/update.js
@@ -32,7 +32,7 @@ function update (args, cb) {
         , dep = ww[1]
         , want = ww[3]
         , what = dep + "@" + want
-        , req = ww[4]
+        , req = ww[5]
         , url = require('url')
 
       // use the initial installation method (repo, tar, git) for updating

test/tap/outdated-git.js
@@ -1,0 +1,28 @@
+var common = require("../common-tap.js")
+var test = require("tap").test
+var npm = require("../../")
+var mkdirp = require("mkdirp")
+var rimraf = require("rimraf")
+var mr = require("npm-registry-mock")
+
+// config
+var pkg = __dirname + "/outdated-git"
+mkdirp.sync(pkg + "/cache")
+
+
+test("dicovers new versions in outdated", function (t) {
+  process.chdir(pkg)
+  t.plan(3)
+  npm.load({cache: pkg + "/cache", registry: common.registry}, function () {
+    npm.outdated(function (er, d) {
+      t.equal('git', d[0][3])
+      t.equal('git', d[0][4])
+      t.equal('git://github.com/robertkowalski/foo-private.git', d[0][5])
+    })
+  })
+})
+
+test("cleanup", function (t) {
+  rimraf.sync(pkg + "/cache")
+  t.end()
+})