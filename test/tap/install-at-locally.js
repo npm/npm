a9eb4d2e2080647aaed671d8d8d974cb933b1089
Alex Kocharin <alex@kocharin.ru>
2014-02-13 14:52:52 +0400

    rewriting a test

--- test/tap/install-at-locally.js
+++ test/tap/install-at-locally.js
@@ -6,25 +6,34 @@ var path = require('path')
 var fs = require('fs')
 var rimraf = require('rimraf')
 var mkdirp = require('mkdirp')
-var pkg = path.join(__dirname, 'install@something')
+var pkg = path.join(__dirname, 'install-at-locally')
 
 test('"npm install ./package@1.2.3" should install local pkg', function(t) {
+  setup()
   t.plan(1)
   npm.load(function() {
-    var _stat = fs.stat
-    fs.stat = function(file) {
-      if (file === './package@1.2.3') {
-        t.ok(true, 'npm is running fs.stat("./package@1.2.3")')
-        fs.stat = _stat
-        t.end()
-      } else {
-        _stat.apply(fs, arguments)
-      }
-    }
-
-    npm.commands.cache.add('./package@1.2.3', function(err) {
-      t.fail('something weird is happening')
+    npm.commands.install(['./package@1.2.3'], function(err) {
+      var p = path.resolve(pkg, 'node_modules/install-at-locally/package.json')
+      t.ok(JSON.parse(fs.readFileSync(p, 'utf8')))
+      t.end()
     })
   })
+})
+
+function setup() {
+  mkdirp.sync(pkg)
+  mkdirp.sync(path.resolve(pkg, 'node_modules'))
+  mkdirp.sync(path.resolve(pkg, 'package@1.2.3'))
+  fs.writeFileSync(path.resolve(pkg, 'package@1.2.3/package.json'), JSON.stringify({
+    name: 'install-at-locally',
+    version: '0.0.0',
+    description: 'Test for 404-parent',
+  }), 'utf8')
+  process.chdir(pkg)
+}
+
+test('cleanup', function(t) {
+  rimraf.sync(pkg)
+  t.end()
 })
 
