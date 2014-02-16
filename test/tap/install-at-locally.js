f851b79a71d9a5f5125aa85877c94faaf91bea5f
isaacs <i@izs.me>
2014-02-15 22:55:05 -0800

    Check for file-ness in /-containing install args
    And only *then* handle the @-ness of it
    
    This also allows for stuff like foo/bar@./path or foo/bar@http://...
    for a hyothetical future when / is allowed in package names, if that
    ever happens.
    
    Re #4666

--- test/tap/install-at-locally.js
+++ test/tap/install-at-locally.js
@@ -8,9 +8,14 @@ var rimraf = require('rimraf')
 var mkdirp = require('mkdirp')
 var pkg = path.join(__dirname, 'install-at-locally')
 
+test("setup", function (t) {
+  mkdirp.sync(pkg)
+  mkdirp.sync(path.resolve(pkg, 'node_modules'))
+  process.chdir(pkg)
+  t.end()
+})
+
 test('"npm install ./package@1.2.3" should install local pkg', function(t) {
-  setup()
-  t.plan(1)
   npm.load(function() {
     npm.commands.install(['./package@1.2.3'], function(err) {
       var p = path.resolve(pkg, 'node_modules/install-at-locally/package.json')
@@ -20,11 +25,15 @@ test('"npm install ./package@1.2.3" should install local pkg', function(t) {
   })
 })
 
-function setup() {
-  mkdirp.sync(pkg)
-  mkdirp.sync(path.resolve(pkg, 'node_modules'))
-  process.chdir(pkg)
-}
+test('"npm install install/at/locally@./package@1.2.3" should install local pkg', function(t) {
+  npm.load(function() {
+    npm.commands.install(['./package@1.2.3'], function(err) {
+      var p = path.resolve(pkg, 'node_modules/install-at-locally/package.json')
+      t.ok(JSON.parse(fs.readFileSync(p, 'utf8')))
+      t.end()
+    })
+  })
+})
 
 test('cleanup', function(t) {
   process.chdir(__dirname)
