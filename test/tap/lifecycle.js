89bf6f260ad018263ff043c68941b3f6fb4c26aa
Robert Kowalski <rok@kowalski.gd>
2014-04-18 14:08:34 +0200

    Fix lifecycle test
    The test in 8b5e7b6a fails as it depends on a unconfigured npm
    which uses exact the same values as a vanilla npmconf instance.
    
    This fix also does not depends on the external dependency npmconf.

--- test/tap/lifecycle.js
+++ test/tap/lifecycle.js
@@ -1,19 +1,12 @@
 var test = require("tap").test
 var npm = require('../../')
-var path = require("path")
-var pkg = path.resolve(__dirname, "..", "..")
 var lifecycle = require('../../lib/utils/lifecycle')
-var Conf = require("npmconf").Conf
 
 test("lifecycle: make env correctly", function (t) {
-  npm.load(function() {
-    var env = lifecycle.makeEnv(pkg)
+  npm.load({enteente: Infinity}, function() {
+    var env = lifecycle.makeEnv({}, null, process.env)
 
-    var conf = new Conf()
-    conf.addEnv(env)
-    conf.keys.forEach(function (key) {
-      t.equal(conf.get(key), npm.config.get(key))
-    })
+    t.equal('Infinity', env.npm_config_enteente)
     t.end()
   })
 })
