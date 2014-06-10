43801b830b9a68bcc51228d2d4403542e0657ee0
isaacs <i@izs.me>
2014-06-10 15:56:35 -0700

    graceful-fs@3.0.2

--- node_modules/graceful-fs/fs.js
+++ node_modules/graceful-fs/fs.js
@@ -2,4 +2,10 @@
 // more evil than monkey-patching the native builtin?
 // Not sure.
 
-eval(process.binding('natives').fs)
+var mod = require("module")
+var pre = '(function (exports, require, module, __filename, __dirname) { '
+var post = '});'
+var src = pre + process.binding('natives').fs + post
+var vm = require('vm')
+var fn = vm.runInThisContext(src)
+return fn(exports, require, module, __filename, __dirname)
