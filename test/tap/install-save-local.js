5c71f8b24485bc9b2982ccc5b4cc32126a4afc1c
Dylan Greene <dylang@gmail.com>
2014-07-25 17:54:01 -0400

    when saving, using file://

--- test/tap/install-save-local.js
+++ test/tap/install-save-local.js
@@ -25,12 +25,9 @@ test('"npm install --save ../local/path" should install local package and save t
 
       var pkgJson = JSON.parse(fs.readFileSync(pkg + '/package.json', 'utf8'))
       t.deepEqual(pkgJson.dependencies, {
-                  'package-local-dependency': '../package-local-dependency'
+                  'package-local-dependency': 'file:///../package-local-dependency'
                 })
       npm.config.set('save', undefined)
-
-      //var devDependencyPackageJson = path.resolve(pkgDir, 'node_modules/package-local-dev-dependency/package.json')
-      //t.ok(JSON.parse(fs.readFileSync(devDependencyPackageJson, 'utf8')))
 
       t.end()
     })
@@ -49,7 +46,7 @@ test('"npm install --save-dev ../local/path" should install local package and sa
 
       var pkgJson = JSON.parse(fs.readFileSync(pkg + '/package.json', 'utf8'))
       t.deepEqual(pkgJson.devDependencies, {
-                  'package-local-dev-dependency': '../package-local-dev-dependency'
+                  'package-local-dev-dependency': 'file:///../package-local-dev-dependency'
                 })
       npm.config.set('save', undefined)
 
