d987707442409f75ab0fa7f346a036ea5787754a
Forrest L Norvell <forrest@npmjs.com>
2014-08-07 17:56:53 -0700

    move fetch into npm-registry-client
    The primary purpose is so that everything doing network requests
    includes auth, if so required. Sends bearer token by default, will send
    HTTP Basic auth with `always-auth` set in the config.

---
+++ node_modules/npm-registry-client/lib/authify.js
@@ -0,0 +1,25 @@
+var url = require("url")
+
+module.exports = authify
+
+function authify (authed, parsed, headers) {
+  var c = this.conf.getCredentialsByURI(url.format(parsed))
+
+  if (c && c.token) {
+    this.log.verbose("request", "using bearer token for auth")
+    headers.authorization = "Bearer " + c.token
+
+    return null
+  }
+
+  if (authed) {
+    if (c && c.username && c.password) {
+      parsed.auth = c.username + ":" + c.password
+    }
+    else {
+      return new Error(
+        "This request requires auth credentials. Run `npm login` and repeat the request."
+      )
+    }
+  }
+}
