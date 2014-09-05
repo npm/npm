6c2721ba1914b3c4ecbf4218578f45fc357ae299
Forrest L Norvell <forrest@npmjs.com>
2014-09-04 22:24:27 -0700

    style tweaks: semicolons, spacing, ' -> "

--- test/tap/cache-add-localdir-fallback.js
+++ test/tap/cache-add-localdir-fallback.js
@@ -1,31 +1,31 @@
 var path = require("path")
 var test = require("tap").test
 var npm = require("../../lib/npm.js")
-var requireInject = require('require-inject');
+var requireInject = require("require-inject")
 
-npm.load({}, function () {
+npm.load({loglevel : "silent"}, function () {
   var resolved = path.resolve(".", "dir-with-package")
   var resolvedPackage = path.join(resolved, "package.json")
 
   var cache = requireInject("../../lib/cache.js", {
     "graceful-fs": {
       stat: function (file, cb) {
-        process.nextTick(function() {
+        process.nextTick(function () {
           switch (file) {
           case "named":
             cb(new Error("ENOENT"))
             break
           case "file.tgz":
-            cb(null,{isDirectory:function(){return false}})
+            cb(null, { isDirectory: function () { return false } })
             break
           case "dir-no-package":
-            cb(null,{isDirectory:function(){return true}})
+            cb(null, { isDirectory: function () { return true } })
             break
           case "dir-no-package/package.json":
             cb(new Error("ENOENT"))
             break
           case "dir-with-package":
-            cb(null,{isDirectory:function(){return true}})
+            cb(null, { isDirectory: function () { return true } })
             break
           case "dir-with-package/package.json":
             cb(null, {})
@@ -37,30 +37,37 @@ npm.load({}, function () {
             cb(null, {})
             break
           default:
-            console.error("Unknown test file passed to stat:",file)
-            process.exit(1)
+            throw new Error("Unknown test file passed to stat: " + file)
           }
         })
       }
     },
-    "../../lib/cache/add-named.js": function addNamed (name,version,data,cb) {
-      cb(null,"addNamed")
+    "../../lib/cache/add-named.js": function addNamed (name, version, data, cb) {
+      cb(null, "addNamed")
     },
-    "../../lib/cache/add-local.js": function addLocal (name,data,cb) {
-      cb(null,"addLocal")
+    "../../lib/cache/add-local.js": function addLocal (name, data, cb) {
+      cb(null, "addLocal")
     }
   })
 
-  test("npm install localdir fallback", function(t) {
+  test("npm install localdir fallback", function (t) {
     t.plan(4)
-    cache.add("named", null, false, function (er,which){
-      t.is(which, "addNamed", "registry package name") })
-    cache.add("file.tgz", null, false, function (er,which){
-      t.is(which, "addLocal", "local file") })
-    cache.add("dir-no-package", null, false, function (er,which){
-      t.is(which, "addNamed", "local directory w/o package.json") })
-    cache.add("dir-with-package", null, false, function (er,which){
-      t.is(which,"addLocal", "local directory w/ package.json") })
+    cache.add("named", null, false, function (er, which) {
+      t.ifError(er, "named was cached")
+      t.is(which, "addNamed", "registry package name")
+    })
+    cache.add("file.tgz", null, false, function (er, which) {
+      t.ifError(er, "file.tgz was cached")
+      t.is(which, "addLocal", "local file")
+    })
+    cache.add("dir-no-package", null, false, function (er, which) {
+      t.ifError(er, "local directory was cached")
+      t.is(which, "addNamed", "local directory w/o package.json")
+    })
+    cache.add("dir-with-package", null, false, function (er, which) {
+      t.ifError(er, "local directory with package was cached")
+      t.is(which,"addLocal", "local directory with package.json")
+    })
     cache.add("file:./dir-with-package", null, false, function (er, which) {
       t.ifError(er, "local directory (as URI) with package was cached")
       t.is(which, "addLocal", "file: URI to local directory with package.json")
