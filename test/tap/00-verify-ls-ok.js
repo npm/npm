818a2ce8300acc8f56befd38e5b625aeec1d0125
Chris Meyers <chris.meyers.fsu@gmail.com>
2014-09-30 10:25:59 -0400

    test was using t.notOk() to ensure value was non-zero. This is not correct because .notOk() evaluates true/false and 1 results in true. Thus, notOk() would fail on the value of 1

--- test/tap/00-verify-ls-ok.js
+++ test/tap/00-verify-ls-ok.js
@@ -5,11 +5,14 @@ var path = require("path")
 var cwd = path.resolve(__dirname, "..", "..")
 var npm = path.resolve(cwd, "cli.js")
 var spawn = require("child_process").spawn
+var fs = require('fs')
 
 test("npm ls in npm", function (t) {
+  t.ok(fs.existsSync(cwd))
+
   var opt = { cwd: cwd, stdio: [ "ignore", "ignore", 2 ] }
   common.npm(['ls'], opt, function(err, code) {
-    t.notOk(code)
+    t.ok(code > 0)
     t.end()
   })
 })
