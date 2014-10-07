0dcd61446335eaf541bf5f2d5186ec1419f86a42
Chris Meyers <chris.meyers.fsu@gmail.com>
2014-10-06 22:14:44 -0400

    changed spawn to common.npm

--- test/tap/cache-shasum-fork.js
+++ test/tap/cache-shasum-fork.js
@@ -1,4 +1,3 @@
-var npm = require.resolve("../../")
 var test = require("tap").test
 var path = require("path")
 var fs = require("fs")
@@ -8,7 +7,6 @@ var mr = require("npm-registry-mock")
 var common = require("../common-tap.js")
 var cache = path.resolve(__dirname, "cache-shasum-fork", "CACHE")
 var cwd = path.resolve(__dirname, "cache-shasum-fork", "CWD")
-var spawn = require("child_process").spawn
 var server
 
 // Test for https://github.com/npm/npm/issues/3265
@@ -30,27 +28,19 @@ test("npm cache - install from fork", function (t) {
   // (but is actually a fork)
   var forkPath = path.resolve(
     __dirname, "cache-shasum-fork", "underscore-1.5.1.tgz")
-  var output = ""
-    , child = spawn(process.execPath, [npm, "install", forkPath], {
+  common.npm(["install", forkPath], {
       cwd: cwd,
       env: {
         "npm_config_cache"    : cache,
         "npm_config_registry" : common.registry,
         "npm_config_loglevel" : "silent"
       }
-    })
+  }, function (err, code, stdout, stderr) {
+    t.ifErr(err, "install finished without error")
+    t.notOk(stderr, "Should not get data on stderr: " + stderr)
+    t.equal(code, 0, "install finished successfully")
 
-  child.stderr.on("data", function (d) {
-    t.fail("Should not get data on stderr: " + d)
-  })
-
-  child.stdout.on("data", function (d) {
-    output += d.toString()
-  })
-
-  child.on("close", function (code) {
-    t.equal(code, 0, "exit ok")
-    t.equal(output, "underscore@1.5.1 node_modules/underscore\n")
+    t.equal(stdout, "underscore@1.5.1 node_modules/underscore\n")
     var index = fs.readFileSync(
       path.join(cwd, "node_modules", "underscore", "index.js"),
       "utf8"
@@ -64,27 +54,18 @@ test("npm cache - install from origin", function (t) {
   // Now install the real 1.5.1.
   rimraf.sync(path.join(cwd, "node_modules"))
   mkdirp.sync(path.join(cwd, "node_modules"))
-  var output = ""
-    , child = spawn(process.execPath, [npm, "install", "underscore"], {
+  common.npm(["install", "underscore"], {
       cwd: cwd,
       env: {
         "npm_config_cache"    : cache,
         "npm_config_registry" : common.registry,
         "npm_config_loglevel" : "silent"
       }
-    })
-
-  child.stderr.on("data", function (d) {
-    t.fail("Should not get data on stderr: " + d)
-  })
-
-  child.stdout.on("data", function (d) {
-    output += d.toString()
-  })
-
-  child.on("close", function (code) {
-    t.equal(code, 0, "exit ok")
-    t.equal(output, "underscore@1.5.1 node_modules/underscore\n")
+  }, function (err, code, stdout, stderr) {
+    t.ifErr(err, "install finished without error")
+    t.equal(code, 0, "install finished successfully")
+    t.notOk(stderr, "Should not get data on stderr: " + stderr)
+    t.equal(stdout, "underscore@1.5.1 node_modules/underscore\n")
     var index = fs.readFileSync(
       path.join(cwd, "node_modules", "underscore", "index.js"),
       "utf8"
