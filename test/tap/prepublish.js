2121f96154bf11c2abc8a299ef347eaefd0c0a85
Chris Meyers <chris.meyers.fsu@gmail.com>
2014-10-06 21:57:09 -0400

    updated prepublish to use common.npm

--- test/tap/prepublish.js
+++ test/tap/prepublish.js
@@ -1,14 +1,18 @@
 // verify that prepublish runs on pack and publish
+var common = require("../common-tap")
 var test = require("tap").test
 var fs = require("graceful-fs")
 var join = require("path").join
 var mkdirp = require("mkdirp")
 var rimraf = require("rimraf")
-var path = require("path")
 
 var pkg = join(__dirname, "prepublish_package")
 var tmp = join(pkg, "tmp")
 var cache = join(pkg, "cache")
+
+function cleanup() {
+  rimraf.sync(pkg)
+}
 
 test("setup", function (t) {
   var n = 0
@@ -38,10 +42,8 @@ test("setup", function (t) {
 })
 
 test("test", function (t) {
-  var spawn = require("child_process").spawn
-  var node = process.execPath
-  var npm = path.resolve(__dirname, "../../cli.js")
-  var env = {
+  var execOpts = { cwd: pkg }
+  execOpts.env = {
     "npm_config_cache"  : cache,
     "npm_config_tmp"    : tmp,
     "npm_config_prefix" : pkg,
@@ -49,46 +51,29 @@ test("test", function (t) {
   }
   for (var i in process.env) {
     if (!/^npm_config_/.test(i))
-      env[i] = process.env[i]
+      execOpts.env[i] = process.env[i]
   }
-  var child = spawn(node, [npm, "pack"], {
-    cwd: pkg,
-    env: env
-  })
-  child.stdout.setEncoding("utf8")
-  child.stderr.on("data", onerr)
-  child.stdout.on("data", ondata)
-  child.on("close", onend)
-  var c = ""
-    , e = ""
-  function ondata (chunk) {
-    c += chunk
-  }
-  function onerr (chunk) {
-    e += chunk
-  }
-  function onend () {
-    if (e) {
-      throw new Error("got stderr data: " + JSON.stringify("" + e))
-    }
-    c = c.trim()
+
+  var child = common.npm(["pack"], execOpts, function(err, code, stdout, stderr) {
+    t.equal(code, 0, "pack finished successfully")
+    t.ifErr(err, "pack finished successfully")
+
+    t.notOk(stderr, "got stderr data:" + JSON.stringify("" + stderr))
+    var c = stdout.trim()
     var regex = new RegExp("" +
       "> npm-test-prepublish@1.2.5 prepublish [^\\r\\n]+\\r?\\n" +
       "> echo ok\\r?\\n" +
       "\\r?\\n" +
       "ok\\r?\\n" +
       "npm-test-prepublish-1.2.5.tgz", "ig")
-
     t.ok(c.match(regex))
     t.end()
-  }
+  })
+  child.stdout.setEncoding("utf8")
 })
 
 test("cleanup", function (t) {
-  rimraf(pkg, function (er) {
-    if (er) throw er
-
-    t.pass("cleaned up")
-    t.end()
-  })
+  cleanup()
+  t.pass("cleaned up")
+  t.end()
 })
