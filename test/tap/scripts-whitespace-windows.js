8447536b2df5fd401c42d8eb8fffa44c73659256
Chris Meyers <chris.meyers.fsu@gmail.com>
2014-10-08 08:48:00 -0400

    updated to use common.npm() and got rid of console pollution

--- test/tap/scripts-whitespace-windows.js
+++ test/tap/scripts-whitespace-windows.js
@@ -1,6 +1,6 @@
 var test = require("tap").test
+var common = require("../common-tap")
 var path = require("path")
-var npm = path.resolve(__dirname, "../../cli.js")
 var pkg = path.resolve(__dirname, "scripts-whitespace-windows")
 var tmp = path.resolve(pkg, "tmp")
 var cache = path.resolve(pkg, "cache")
@@ -9,63 +9,46 @@ var dep = path.resolve(pkg, "dep")
 
 var mkdirp = require("mkdirp")
 var rimraf = require("rimraf")
-var node = process.execPath
-var spawn = require("child_process").spawn
 
 test("setup", function (t) {
+  cleanup()
   mkdirp.sync(cache)
   mkdirp.sync(tmp)
-  rimraf.sync(modules)
 
-  var env = {
-    "npm_config_cache": cache,
-    "npm_config_tmp": tmp,
-    "npm_config_prefix": pkg,
-    "npm_config_global": "false"
-  }
-
-  var child = spawn(node, [npm, "i", dep], {
+  common.npm(["i", dep], {
     cwd: pkg,
-    env: env
-  })
-
-  child.stdout.setEncoding("utf8")
-  child.stderr.on("data", function (chunk) {
-    t.fail("got stderr data: " + JSON.stringify("" + chunk))
-  })
-  child.on("close", function () {
+    env: {
+      "npm_config_cache": cache,
+      "npm_config_tmp": tmp,
+      "npm_config_prefix": pkg,
+      "npm_config_global": "false"
+    }
+  }, function (err, code, stdout, stderr) {
+    t.ifErr(err, "npm i " + dep + " finished without error")
+    t.equal(code, 0, "npm i " + dep + " exited ok")
+    t.notOk(stderr, "no output stderr")
     t.end()
   })
 })
 
 test("test", function (t) {
-
-  var child = spawn(node, [npm, "run", "foo"], {
-    cwd: pkg,
-    env: process.env
-  })
-
-  child.stdout.setEncoding("utf8")
-  child.stderr.on("data", function (chunk) {
-    t.fail("got stderr data: " + JSON.stringify("" + chunk))
-  })
-  child.stdout.on("data", ondata)
-  child.on("close", onend)
-  var c = ""
-  function ondata (chunk) {
-    c += chunk
-  }
-  function onend () {
-    c = c.trim()
-
-    t.ok(/npm-test-fine/.test(c))
+  common.npm(["run", "foo"], { cwd: pkg }, function (err, code, stdout, stderr) {
+    t.ifErr(err, "npm run finished without error")
+    t.equal(code, 0, "npm run exited ok")
+    t.notOk(stderr, "no output stderr: ", stderr)
+    stdout = stdout.trim()
+    t.ok(/npm-test-fine/.test(stdout))
     t.end()
-  }
+  })
 })
 
 test("cleanup", function (t) {
+  cleanup()
+  t.end()
+})
+
+function cleanup() {
   rimraf.sync(cache)
   rimraf.sync(tmp)
   rimraf.sync(modules)
-  t.end()
-})
+}
