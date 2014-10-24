a1148702f53f82d49606b2e4dac7581261fff442
Forrest L Norvell <forrest@npmjs.com>
2014-10-23 23:04:57 -0700

    perl 5.18.x doesn't like -pi without filenames

--- scripts/doc-build.sh
+++ scripts/doc-build.sh
@@ -72,17 +72,17 @@ html_replace_tokens () {
 	| sed "s|@DATE@|$date|g" \
 	| sed "s|@URL@|$url|g" \
 	| sed "s|@VERSION@|$version|g" \
-	| perl -pi -e 's/<h1([^>]*)>([^\(]*\([0-9]\)) -- (.*?)<\/h1>/<h1>\2<\/h1> <p>\3<\/p>/g' \
-	| perl -pi -e 's/npm-npm/npm/g' \
-	| perl -pi -e 's/([^"-])(npm-)?README(?!\.html)(\(1\))?/\1<a href="..\/..\/doc\/README.html">README<\/a>/g' \
-	| perl -pi -e 's/<title><a href="[^"]+README.html">README<\/a><\/title>/<title>README<\/title>/g' \
-	| perl -pi -e 's/([^"-])([^\(> ]+)(\(1\))/\1<a href="..\/cli\/\2.html">\2\3<\/a>/g' \
-	| perl -pi -e 's/([^"-])([^\(> ]+)(\(3\))/\1<a href="..\/api\/\2.html">\2\3<\/a>/g' \
-	| perl -pi -e 's/([^"-])([^\(> ]+)(\(5\))/\1<a href="..\/files\/\2.html">\2\3<\/a>/g' \
-	| perl -pi -e 's/([^"-])([^\(> ]+)(\(7\))/\1<a href="..\/misc\/\2.html">\2\3<\/a>/g' \
-	| perl -pi -e 's/\([1357]\)<\/a><\/h1>/<\/a><\/h1>/g' \
+	| perl -p -e 's/<h1([^>]*)>([^\(]*\([0-9]\)) -- (.*?)<\/h1>/<h1>\2<\/h1> <p>\3<\/p>/g' \
+	| perl -p -e 's/npm-npm/npm/g' \
+	| perl -p -e 's/([^"-])(npm-)?README(?!\.html)(\(1\))?/\1<a href="..\/..\/doc\/README.html">README<\/a>/g' \
+	| perl -p -e 's/<title><a href="[^"]+README.html">README<\/a><\/title>/<title>README<\/title>/g' \
+	| perl -p -e 's/([^"-])([^\(> ]+)(\(1\))/\1<a href="..\/cli\/\2.html">\2\3<\/a>/g' \
+	| perl -p -e 's/([^"-])([^\(> ]+)(\(3\))/\1<a href="..\/api\/\2.html">\2\3<\/a>/g' \
+	| perl -p -e 's/([^"-])([^\(> ]+)(\(5\))/\1<a href="..\/files\/\2.html">\2\3<\/a>/g' \
+	| perl -p -e 's/([^"-])([^\(> ]+)(\(7\))/\1<a href="..\/misc\/\2.html">\2\3<\/a>/g' \
+	| perl -p -e 's/\([1357]\)<\/a><\/h1>/<\/a><\/h1>/g' \
 	| (if [ $(basename $(dirname $dest)) == "doc" ]; then
-			perl -pi -e 's/ href="\.\.\// href="/g'
+			perl -p -e 's/ href="\.\.\// href="/g'
 		else
 			cat
 		fi)
@@ -90,11 +90,11 @@ html_replace_tokens () {
 
 man_replace_tokens () {
 	sed "s|@VERSION@|$version|g" \
-	| perl -pi -e 's/(npm\\-)?([a-zA-Z\\\.\-]*)\(1\)/npm help \2/g' \
-	| perl -pi -e 's/(npm\\-)?([a-zA-Z\\\.\-]*)\(([57])\)/npm help \3 \2/g' \
-	| perl -pi -e 's/(npm\\-)?([a-zA-Z\\\.\-]*)\(3\)/npm apihelp \2/g' \
-	| perl -pi -e 's/npm\(1\)/npm help npm/g' \
-	| perl -pi -e 's/npm\(3\)/npm apihelp npm/g'
+	| perl -p -e 's/(npm\\-)?([a-zA-Z\\\.\-]*)\(1\)/npm help \2/g' \
+	| perl -p -e 's/(npm\\-)?([a-zA-Z\\\.\-]*)\(([57])\)/npm help \3 \2/g' \
+	| perl -p -e 's/(npm\\-)?([a-zA-Z\\\.\-]*)\(3\)/npm apihelp \2/g' \
+	| perl -p -e 's/npm\(1\)/npm help npm/g' \
+	| perl -p -e 's/npm\(3\)/npm apihelp npm/g'
 }
 
 case $dest in
