90326282e3232e3241316ac3336d0989a1573b00
Forrest L Norvell <forrest@npmjs.com>
2014-11-19 16:18:35 -0800

    update test for new behavior

--- test/tap/gently-rm-overeager.js
+++ test/tap/gently-rm-overeager.js
@@ -32,9 +32,11 @@ test('cache add', function (t) {
     t.ok(c, 'test-whoops install also failed')
     fs.readdir(pkg, function (er, files) {
       t.ifError(er, 'package directory is still there')
-      t.deepEqual(files, ['npm-debug.log'], 'only debug log remains')
-
-      t.end()
+      t.deepEqual(files, [], 'only debug log remains')
+      fs.exists(resolve(common.npm_config_cache, 'npm-debug.log'), function (p) {
+        t.ok(p, 'debug log is saved')
+        t.end()
+      })
     })
   })
 })
