0d314256029a9df1cb7ccc2a5237b74008ea44ed
gcb <descartavel1@gmail.com>
2015-01-06 01:18:05 +0000

    correct test description

--- test/tap/run-script.js
+++ test/tap/run-script.js
@@ -89,7 +89,7 @@ test('npm run-script test', function (t) {
 
 test('npm run-script env', function (t) {
   common.npm(['run-script', 'env'], opts, function (er, code, stdout, stderr) {
-    t.ifError(er, 'failed to find default env script');
+    t.ifError(er, 'using default env script');
     t.notOk(stderr, 'should not generate errors')
     t.ok( stdout.indexOf('npm_config_init_version') > 0, 'expected values in var list' )
     t.end()
