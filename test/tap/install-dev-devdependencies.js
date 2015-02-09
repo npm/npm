ad03e0696b307d39e667b3c2d0457bb54452f426
smikes <smikes@cubane.com>
2015-02-09 06:02:19 -0700

    test: more tests of `npm install` when package version specified
    This expands on the "failing test for 3504" purpose.
    
    Now uses npm-mock-registry.
    
    Add test (install-dependencies) that demonstrates `npm install`
    looks into package.json for "dependencies" and honors version.
    
    Cleaned up test (install-devdependencies) that shows `npm install`
    does not honor version from `devDependencies` if no --dev
    
    Add test that's intended to show that `npm install --dev checker`
    would honor the semver from devDependencies; unfortunately this
    test fails because `npm install --dev checker` wants mocha
    which is not in mock-registry

---
+++ test/tap/install-dev-devdependencies.js
@@ -0,0 +1,74 @@
+var common = require('../common-tap.js')
+var test = require('tap').test
+var npm = require('../../')
+var path = require('path')
+var fs = require('fs')
+var rimraf = require('rimraf')
+var mkdirp = require('mkdirp')
+var mr = require('npm-registry-mock')
+var readJson = require('read-package-json')
+var osenv = require('osenv')
+
+var pkg = path.join(__dirname, 'install-dev-devdependencies')
+var depJson = path.resolve(pkg, 'node_modules', 'checker', 'package.json')
+var regOpts = {
+  port: common.port
+}
+var npmOpts = {
+  registry: common.registry,
+  prefix: pkg,
+  cache: path.resolve(pkg, 'cache')
+}
+
+var server
+
+function resetPackage (opts) {
+  process.chdir(osenv.tmpdir())
+  rimraf.sync(pkg)
+  mkdirp.sync(pkg)
+  process.chdir(pkg)
+
+  rimraf.sync('node_modules')
+  mkdirp.sync('cache')
+
+  var p = {}
+
+  for (var key in opts) {
+    p[key] = { 'checker': opts[key] }
+  }
+
+  fs.writeFileSync('package.json', JSON.stringify(p, null, 2) + '\n', 'utf8')
+}
+
+test('setup', function (t) {
+  mr(regOpts, function (err, s) {
+    server = s
+    t.ifError(err, 'server started ok')
+    t.end()
+  })
+})
+
+test('npm install checker should install 0.5.1 if specified in `devDependencies`', function (t) {
+  resetPackage({ 'devDependencies': '0.5.1' })
+
+  npmOpts.dev = true
+  npm.load(npmOpts, function (err) {
+    t.ifError(err, 'npm loaded ok')
+    npm.commands.install(['checker'], function (err) {
+      t.ifError(err, 'no error on install')
+      readJson(depJson, function (err, data) {
+        t.ifError(err, 'no error reading installed package.json')
+        t.deepEqual(data.version, '0.5.1')
+        t.end()
+      })
+    })
+  })
+})
+
+test('cleanup', function (t) {
+  server.close()
+
+  process.chdir(__dirname)
+  rimraf.sync(pkg)
+  t.end()
+})
