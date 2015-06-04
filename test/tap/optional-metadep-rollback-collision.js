29d34480872242b4d5fb18bb1fc4cd0451f590cf
Rebecca Turner <me@re-becca.org>
2015-06-04 05:16:25 -0400

    Fixup optional-metadep-rollback-collision test w/ npm@3

--- test/tap/optional-metadep-rollback-collision.js
+++ test/tap/optional-metadep-rollback-collision.js
@@ -10,7 +10,7 @@ var common = require('../common-tap.js')
 
 var pkg = path.resolve(__dirname, 'optional-metadep-rollback-collision')
 var deps = path.resolve(pkg, 'deps')
-var nm = path.resolve(pkg, 'node_modules')
+var opdep = path.resolve(pkg, 'node_modules', 'opdep')
 var cache = path.resolve(pkg, 'cache')
 var pidfile = path.resolve(pkg, 'child.pid')
 
@@ -205,9 +205,12 @@ test('go go test racer', function (t) {
     },
     function (er, code, stdout, stderr) {
       t.ifError(er, 'install ran to completion without error')
-      t.notOk(code, 'npm install exited with code 0')
+      t.is(code, 0, 'npm install exited with code 0')
+      t.is(stderr, '')
 
-      t.equal(stdout, 'ok\nok\n')
+      // stdout should be empty, because we only have one, optional, dep and
+      // if it fails we shouldn't try installing anything
+      t.equal(stdout, '')
       t.notOk(/not ok/.test(stdout), 'should not contain the string \'not ok\'')
       t.end()
     }
@@ -216,7 +219,7 @@ test('go go test racer', function (t) {
 
 test('verify results', function (t) {
   t.throws(function () {
-    fs.statSync(nm)
+    fs.statSync(opdep)
   })
   t.end()
 })
