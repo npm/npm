7bb37bca510d04a631fbdb5abc600a03a3f9a3c4
Forrest L Norvell <forrest@npmjs.com>
2015-06-24 21:59:59 -0700

    lint: "Unexpected space before unary operator x."

--- test/tap/version-no-git.js
+++ test/tap/version-no-git.js
@@ -24,7 +24,7 @@ test('npm version <semver> in a git repo without the git binary', function (t) {
     })
 
     version(['patch'], function (err) {
-      if (! t.error(err)) return t.end()
+      if (!t.error(err)) return t.end()
       var p = path.resolve(pkg, 'package')
       var testPkg = require(p)
       t.equal('0.0.1', testPkg.version, '\'' + testPkg.version + '\' === \'0.0.1\'')
