a9f6fe9dc558f17c4a7b9eb83329ac080f7df4b7
Kat Marchán <kzm@sykosomatic.org>
2016-01-07 16:11:14 -0800

    test: git servers take a while to shut down
    Specially on travis -- changing ports prevents that conflict
    from happening, but we'll need to keep doing it for every test
    that uses git.
    
    PR-URL: https://github.com/npm/npm/pull/10903
    Credit: @zkat

--- test/tap/git-dependency-install-link.js
+++ test/tap/git-dependency-install-link.js
@@ -30,7 +30,7 @@ var pjParent = JSON.stringify({
   name: 'parent',
   version: '1.2.3',
   dependencies: {
-    'child': 'git://localhost:1234/child.git'
+    'child': 'git://localhost:1243/child.git'
   }
 }, null, 2) + '\n'
 
@@ -135,7 +135,7 @@ function setup (cb) {
             '--listen=localhost',
             '--export-all',
             '--base-path=.',
-            '--port=1234'
+            '--port=1243'
           ],
           {
             cwd: pkg,
