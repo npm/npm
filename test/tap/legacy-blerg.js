2e5e1ab5c075e2900080247b8c7ee4154b2a4d72
Rebecca Turner <me@re-becca.org>
2016-01-26 15:16:04 -0800

    test: Add machine converted legacy tests

---
+++ test/tap/legacy-blerg.js
@@ -0,0 +1,77 @@
+'use strict'
+var test = require('tap').test
+var common = require('../common-tap.js')
+var path = require('path')
+var rimraf = require('rimraf')
+var mkdirp = require('mkdirp')
+var basepath = path.resolve(__dirname, path.basename(__filename, '.js'))
+var fixturepath = path.resolve(basepath, 'npm-test-blerg')
+var modulepath = path.resolve(basepath, 'node_modules')
+var installedpath = path.resolve(modulepath, 'npm-test-blerg')
+var Tacks = require('tacks')
+var File = Tacks.File
+var Dir = Tacks.Dir
+var fixture = new Tacks(
+  Dir({
+    README: File(
+      'just an npm test\n'
+    ),
+    'package.json': File({
+      name: 'npm-test-blerg',
+      version: '0.0.2',
+      scripts: {
+        test: 'node test.js'
+      },
+      publishConfig: {
+        tag: 'foo'
+      }
+    }),
+    'test.js': File(
+      "var assert = require('assert')\n" +
+      "assert.equal(undefined, process.env.npm_config__password, 'password exposed!')\n" +
+      "assert.equal(undefined, process.env.npm_config__auth, 'auth exposed!')\n" +
+      "assert.equal(undefined, process.env.npm_config__authCrypt, 'authCrypt exposed!')\n"
+    )
+  })
+)
+test('setup', function (t) {
+  setup()
+  t.done()
+})
+test('blerg', function (t) {
+  common.npm(['install', fixturepath], {cwd: basepath}, installCheckAndTest)
+  function installCheckAndTest (err, code, stdout, stderr) {
+    if (err) throw err
+    console.error(stderr)
+    console.log(stdout)
+    t.is(code, 0, 'install went ok')
+    common.npm(['test'], {cwd: installedpath}, testCheckAndRemove)
+  }
+  function testCheckAndRemove (err, code, stdout, stderr) {
+    if (err) throw err
+    console.error(stderr)
+    console.log(stdout)
+    t.is(code, 0, 'test went ok')
+    common.npm(['rm', fixturepath], {cwd: basepath}, removeCheckAndDone)
+  }
+  function removeCheckAndDone (err, code, stdout, stderr) {
+    if (err) throw err
+    console.error(stderr)
+    console.log(stdout)
+    t.is(code, 0, 'remove went ok')
+    t.done()
+  }
+})
+test('cleanup', function (t) {
+  cleanup()
+  t.done()
+})
+function setup () {
+  cleanup()
+  fixture.create(fixturepath)
+  mkdirp.sync(modulepath)
+}
+function cleanup () {
+  fixture.remove(fixturepath)
+  rimraf.sync(basepath)
+}
---
+++ test/tap/legacy-platform-all.js
@@ -0,0 +1,84 @@
+'use strict'
+var test = require('tap').test
+var common = require('../common-tap.js')
+var path = require('path')
+var rimraf = require('rimraf')
+var mkdirp = require('mkdirp')
+var basepath = path.resolve(__dirname, path.basename(__filename, '.js'))
+var fixturepath = path.resolve(basepath, 'npm-test-platform-all')
+var modulepath = path.resolve(basepath, 'node_modules')
+var installedpath = path.resolve(modulepath, 'npm-test-platform-all')
+var Tacks = require('tacks')
+var File = Tacks.File
+var Dir = Tacks.Dir
+var fixture = new Tacks(
+  Dir({
+    README: File(
+      'just an npm test\n'
+    ),
+    'package.json': File({
+      name: 'npm-test-platform-all',
+      version: '9.9.9-9',
+      homepage: 'http://www.zombo.com/',
+      os: [
+        'darwin',
+        'linux',
+        'win32',
+        'solaris',
+        'haiku',
+        'sunos',
+        'freebsd',
+        'openbsd',
+        'netbsd'
+      ],
+      cpu: [
+        'arm',
+        'mips',
+        'ia32',
+        'x64',
+        'sparc'
+      ]
+    })
+  })
+)
+test('setup', function (t) {
+  setup()
+  t.done()
+})
+test('platform-all', function (t) {
+  common.npm(['install', fixturepath], {cwd: basepath}, installCheckAndTest)
+  function installCheckAndTest (err, code, stdout, stderr) {
+    if (err) throw err
+    console.error(stderr)
+    console.log(stdout)
+    t.is(code, 0, 'install went ok')
+    common.npm(['test'], {cwd: installedpath}, testCheckAndRemove)
+  }
+  function testCheckAndRemove (err, code, stdout, stderr) {
+    if (err) throw err
+    console.error(stderr)
+    console.log(stdout)
+    t.is(code, 0, 'test went ok')
+    common.npm(['rm', fixturepath], {cwd: basepath}, removeCheckAndDone)
+  }
+  function removeCheckAndDone (err, code, stdout, stderr) {
+    if (err) throw err
+    console.error(stderr)
+    console.log(stdout)
+    t.is(code, 0, 'remove went ok')
+    t.done()
+  }
+})
+test('cleanup', function (t) {
+  cleanup()
+  t.done()
+})
+function setup () {
+  cleanup()
+  fixture.create(fixturepath)
+  mkdirp.sync(modulepath)
+}
+function cleanup () {
+  fixture.remove(fixturepath)
+  rimraf.sync(basepath)
+}
