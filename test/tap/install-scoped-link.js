475ceb8c495dffc6a58e151257500cfbe7fc5955
Rebecca Turner <me@re-becca.org>
2016-05-11 15:30:11 -0700

    test fixup

--- test/tap/install-scoped-link.js
+++ test/tap/install-scoped-link.js
@@ -3,9 +3,6 @@ var fs = require('graceful-fs')
 var path = require('path')
 var existsSync = fs.existsSync || path.existsSync
 
-var mkdirp = require('mkdirp')
-var osenv = require('osenv')
-var rimraf = require('rimraf')
 var test = require('tap').test
 
 var escapeExecPath = require('../../lib/utils/escape-exec-path')
