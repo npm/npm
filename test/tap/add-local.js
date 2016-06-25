e5f50ea9f84fe8cac6978d18f7efdf43834928e7
Kat Marchán <kzm@sykosomatic.org>
2016-06-24 18:21:18 -0700

    test: Added tests to verify addLocal race patch
    PR-URL: https://github.com/npm/npm/pull/13023
    Credit: @zkat
    Reviewed-By: @othiym23

--- test/tap/add-local.js
+++ test/tap/add-local.js
@@ -64,6 +64,63 @@ test('addLocal directory race on Windows', function (t) {
   })
 })
 
+test('addLocal temporary cache file race', function (t) {
+  // See https://github.com/npm/npm/issues/12669
+  setup()
+  var p = {
+    name: 'test',
+    version: '1.0.0',
+    type: 'directory',
+    spec: pkg
+  }
+  var fixture = new Tacks(
+    Dir({
+      'package.json': File(p)
+    })
+  )
+  var addLocal = requireInject('../../lib/cache/add-local', {
+    // basic setup/mock stuff
+    '../../lib/npm.js': {
+      cache: cache,
+      tmp: tmp,
+      prefix: prefix
+    },
+    '../../lib/cache/add-local-tarball.js': function (tgz, data, shasum, cb) {
+      cb(null)
+    },
+    '../../lib/utils/lifecycle.js': function (data, cycle, p, cb) {
+      cb(null)
+    },
+    '../../lib/utils/tar.js': {
+      pack: function (tgz, p, data, cb) {
+        cb(null)
+      }
+    },
+    'sha': {
+      get: function (tgz, cb) {
+        cb(null, 'deadbeef')
+      }
+    },
+
+    // Test-specific mocked values to simulate race.
+    '../../lib/cache/get-stat': function (cb) {
+      cb(null, {uid: 1, gid: 2})
+    },
+    chownr: function (x, y, z, cb) {
+      // Simulate a race condition between `tar.pack` and `chownr`
+      // where the latter will return `ENOENT` when an async process
+      // removes a file that its internal `fs.readdir` listed.
+      cb({code: 'ENOENT'})
+    }
+  })
+
+  fixture.create(pkg)
+  addLocal(p, null, function (err) {
+    t.ifErr(err, 'addLocal completed without error')
+    t.done()
+  })
+})
+
 test('cleanup', function (t) {
   cleanup()
   t.done()
