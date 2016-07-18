eb1dc8384e4d25947b9fc87f6e73d332fad26ca1
Aaron Jensen <aaronjensen@gmail.com>
2016-07-18 15:57:48 -0700

    Use TMPDIR if it is writable
    Only fall back to working directory if TMPDIR is not writable, error if
    neither are writable.

--- test/tap/lifecycle.js
+++ test/tap/lifecycle.js
@@ -42,3 +42,30 @@ test('lifecycle : rejects wd for ', function (t) {
     t.end()
   })
 })
+
+test('lifecycle : accepts TMPDIR that is writable', function (t) {
+  var wd = '/opt/my-time/node_modules/time'
+
+  lifecycle._getTemporaryDirectory(wd, process.env, function (er, tmpdir) {
+    t.equal(tmpdir, process.env.TMPDIR)
+    t.end()
+  })
+})
+
+test('lifecycle : uses wd if TMPDIR is not writable', function (t) {
+  var wd = process.cwd()
+
+  lifecycle._getTemporaryDirectory(wd, {TMPDIR: '/probably/not/writable'}, function (er, tmpdir) {
+    t.equal(tmpdir, wd)
+    t.end()
+  })
+})
+
+test('lifecycle : errors if neither TMPDIR or wd are writable', function (t) {
+  var wd = '/probably/not/writable/either'
+
+  lifecycle._getTemporaryDirectory(wd, {TMPDIR: '/probably/not/writable'}, function (er, tmpdir) {
+    t.ok(er, 'non-writable-directories tmpdir should produce an error')
+    t.end()
+  })
+})
