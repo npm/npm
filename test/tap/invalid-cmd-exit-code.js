7dc8b8ba44ae1874cb2871328aaaf76bf6ccda17
Forrest L Norvell <forrest@npmjs.com>
2016-11-04 15:06:07 -0700

    test: tweaks for clean build

--- test/tap/invalid-cmd-exit-code.js
+++ test/tap/invalid-cmd-exit-code.js
@@ -5,7 +5,7 @@ var opts = { cwd: process.cwd() }
 
 test('npm asdf should return exit code 1', function (t) {
   common.npm(['asdf'], opts, function (err, code, stdout, stderr) {
-    if (err) throw er
+    if (err) throw err
     t.ok(code, 'exit code should not be zero')
     if (stdout.trim()) t.comment(stdout.trim())
     if (stderr.trim()) t.comment(stderr.trim())
@@ -15,7 +15,7 @@ test('npm asdf should return exit code 1', function (t) {
 
 test('npm help should return exit code 0', function (t) {
   common.npm(['help'], opts, function (err, code, stdout, stderr) {
-    if (err) throw er
+    if (err) throw err
     t.equal(code, 0, 'exit code should be 0')
     if (stdout.trim()) t.comment(stdout.trim())
     if (stderr.trim()) t.comment(stderr.trim())
@@ -25,7 +25,7 @@ test('npm help should return exit code 0', function (t) {
 
 test('npm help fadf should return exit code 0', function (t) {
   common.npm(['help', 'fadf'], opts, function (err, code, stdout, stderr) {
-    if (err) throw er
+    if (err) throw err
     t.equal(code, 0, 'exit code should be 0')
     if (stdout.trim()) t.comment(stdout.trim())
     if (stderr.trim()) t.comment(stderr.trim())
