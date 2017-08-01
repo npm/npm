4f49f687bbd54b6a0e406936ae35593d8e971e1e
Mike Sherov <mike.sherov@gmail.com>
2017-08-01 09:14:33 -0400

    lifecycle: remove local dependencies other than spawn
    PR-URL: https://github.com/npm/npm/pull/18025
    Credit: @mikesherov
    Reviewed-By: @zkat

--- test/tap/lifecycle.js
+++ test/tap/lifecycle.js
@@ -3,12 +3,39 @@ var npm = require('../../')
 var lifecycle = require('../../lib/utils/lifecycle')
 
 test('lifecycle: make env correctly', function (t) {
-  npm.load({enteente: Infinity}, function () {
-    var env = lifecycle.makeEnv({}, null, process.env)
+  const pkg = {
+    name: 'myPackage',
+    version: '1.0.0',
+    contributors: [{ name: 'Mike Sherov', email: 'beep@boop.com' }]
+  }
+  const config = {
+    enteente: Infinity,
+    '_privateVar': 1,
+    '_myPackage:myPrivateVar': 1,
+    'myPackage:bar': 2,
+    'myPackage:foo': 3,
+    'myPackage@1.0.0:baz': 4,
+    'myPackage@1.0.0:foo': 5
+  }
 
-    t.equal('Infinity', env.npm_config_enteente)
-    t.end()
-  })
+  var env = lifecycle.makeEnv(pkg, { config }, null, process.env)
+
+  t.equal('myPackage', env.npm_package_name, 'package data is included')
+  t.equal('Mike Sherov', env.npm_package_contributors_0_name, 'nested package data is included')
+
+  t.equal('Infinity', env.npm_config_enteente, 'public config is included')
+  t.equal(undefined, env.npm_config_privateVar, 'private config is excluded')
+
+  t.equal('1', env.npm_config_myPackage_myPrivateVar, 'private package config is included by name')
+  t.equal('2', env.npm_config_myPackage_bar, 'public package config is included by name')
+
+  t.equal('5', env.npm_config_myPackage_1_0_0_foo, 'public package@version config is included by name')
+
+  t.equal('1', env.npm_package_config_myPrivateVar, 'package private config is included')
+  t.equal('2', env.npm_package_config_bar, 'package config is included')
+  t.equal('4', env.npm_package_config_baz, 'package@version config is included')
+  t.equal('5', env.npm_package_config_foo, 'package@version config overrides package config')
+  t.end()
 })
 
 test('lifecycle : accepts wd for package that matches project\'s name', function (t) {
